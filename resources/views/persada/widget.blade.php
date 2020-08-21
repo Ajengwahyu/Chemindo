c9b6e107cd9b969922a9d11bb512297142e86e69
Ajengwahyu <Ajengwahyu>
2020-08-21 15:55:15 +0700

    first

---
+++ resources/views/persada/widget.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{('css/widget.css')}}">
+        <title>widget</title>
+    </head>
+    <body>
+
+        <section class="bottoms-header">
+            <div class="info">
+                <h4>
+                    We are the secret behind your <br> successful products
+                </h4>
+                <h5>
+                    We make sure that our customers' demands <br>
+                    are met by providing the highest chemical <br>
+                    products available. 
+                </h5>
+                <h6>
+                    To bring our customer the best source of their flavours, we work <br>
+                    and comply with the world's best brands and manufacturer of <br>
+                    Natural Extracts, Aromatic Chemicals, Natural Flavours and <br>
+                    Essential Oils. We make sure our customer holds the keys to <br>
+                    limitless possibilities for their taste explorations.
+                </h6>
+                <div class="box">
+                    <a href="#"><b>LEARN MORE</b></a>
+                </div>
+            </div>
+        </section>
+
+        <section class="tops-widget">
+            <div class="title-widget">
+                <h5>Why Perdana Chemindo Perkasa as your first choice for chemicals</h5>
+                <div class="sub-title">
+                    <h3>990+</h3><p>Variations of products <br> for you to choose</p>
+
+                    <h3>20+</h3><p>Years of experience in industrial <br> chemicals & fiberglass fabrication</p>
+
+                    <h3>200+</h3><p>Tons of product delivery <br> every month</p>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
